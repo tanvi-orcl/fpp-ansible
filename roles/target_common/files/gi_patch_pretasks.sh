be11b5d925af8f30ce82597479220950a6c86ba0
Tanvi Varadhachary <tanvi.varadhachary@oracle.com>
2021-12-08 13:11:12 -0600

    updating TO EDIT details

--- roles/target_common/files/gi_patch_pretasks.sh
+++ roles/target_common/files/gi_patch_pretasks.sh
@@ -3,6 +3,3 @@ systemctl stop crond
 ./linux/stoptools.sh
 
 # TO EDIT
-
-#wc -l lsof /linux     # if they are not running then it comes back with nothing
-# /ggtrail --> relocate it to a node that's not being patched and then release anything running in ggtrail
